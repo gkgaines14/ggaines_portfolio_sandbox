0d8eb5de657e4bd5056b0fea72f8bf3e6c678d1e
Greg Gaines <gkgaines@gmail.com>
2023-09-05 21:23:33 -0400

    first commit

---
+++ js/app.js
@@ -0,0 +1,202 @@
+import { setTheme, loadListeners } from './functions.js';
+
+//Home Script
+const launchHome = (page) => {
+  window.onload = () => {
+    if (!localStorage.getItem('theme')) {
+      localStorage.setItem('theme', 'dark');
+    }
+    setTheme(localStorage.getItem('theme'));
+    loadListeners();
+    console.log('Home page loaded');
+  };
+
+  const header = document.querySelector('#header');
+  const learnMore = document.querySelector('#learn-more');
+
+  //Learn More button scroll
+  learnMore.addEventListener('click', () => {
+    window.scroll({
+      top: 803,
+      left: 0,
+      behavior: 'smooth',
+    });
+  });
+};
+
+//About Script
+const launchAbout = (page) => {
+  window.onload = () => {
+    if (!localStorage.getItem('theme')) {
+      localStorage.setItem('theme', 'dark');
+    }
+    setTheme(localStorage.getItem('theme'));
+    loadListeners();
+    console.log('About page loaded');
+  };
+
+  const header = document.querySelector('#header');
+};
+
+// Work Page Script
+
+const launchWork = (page) => {
+  window.onload = () => {
+    if (!localStorage.getItem('theme')) {
+      localStorage.setItem('theme', 'dark');
+    }
+    setTheme(localStorage.getItem('theme'));
+    loadListeners();
+    console.log('Work page loaded');
+  };
+
+  const allTab = document.querySelector('#p-all');
+  const sitesTab = document.querySelector('#p-sites');
+  const appsTab = document.querySelector('#p-apps');
+  const codeTab = document.querySelector('#p-code');
+
+  const siteProjects = document.querySelectorAll('.sites');
+  const appProjects = document.querySelectorAll('.apps');
+  const codeProjects = document.querySelectorAll('.code');
+
+  allTab.addEventListener('click', () => {
+    siteProjects.forEach((item) => {
+      item.style.display = 'inline';
+    });
+    appProjects.forEach((item) => {
+      item.style.display = 'inline';
+    });
+    codeProjects.forEach((item) => {
+      item.style.display = 'inline';
+    });
+
+    allTab.className = 'tab active-tab';
+    sitesTab.className = 'tab';
+    appsTab.className = 'tab';
+    codeTab.className = 'tab';
+  });
+
+  sitesTab.addEventListener('click', () => {
+    siteProjects.forEach((item) => {
+      item.style.display = 'inline';
+    });
+    appProjects.forEach((item) => {
+      item.style.display = 'none';
+    });
+    codeProjects.forEach((item) => {
+      item.style.display = 'none';
+    });
+
+    allTab.className = 'tab';
+    sitesTab.className = 'tab active-tab';
+    appsTab.className = 'tab';
+    codeTab.className = 'tab';
+  });
+
+  appsTab.addEventListener('click', () => {
+    siteProjects.forEach((item) => {
+      item.style.display = 'none';
+    });
+    appProjects.forEach((item) => {
+      item.style.display = 'inline';
+    });
+    codeProjects.forEach((item) => {
+      item.style.display = 'none';
+    });
+
+    allTab.className = 'tab';
+    sitesTab.className = 'tab';
+    appsTab.className = 'tab  active-tab';
+    codeTab.className = 'tab';
+  });
+
+  codeTab.addEventListener('click', () => {
+    siteProjects.forEach((item) => {
+      item.style.display = 'none';
+    });
+    appProjects.forEach((item) => {
+      item.style.display = 'none';
+    });
+    codeProjects.forEach((item) => {
+      item.style.display = 'inline';
+    });
+
+    allTab.className = 'tab';
+    sitesTab.className = 'tab';
+    appsTab.className = 'tab';
+    codeTab.className = 'tab active-tab';
+  });
+  // const header = document.querySelector('#header');
+};
+
+//Contact Script
+const launchContact = (page) => {
+  window.onload = () => {
+    if (!localStorage.getItem('theme')) {
+      localStorage.setItem('theme', 'dark');
+    }
+    setTheme(localStorage.getItem('theme'));
+    loadListeners();
+    console.log('About page loaded');
+  };
+
+  const header = document.querySelector('#header');
+  const submitButton = document.querySelector('#submit-btn');
+  const confirmMessage = document.querySelector('.confirm-message');
+  const imageBack = document.querySelector('#mail-icon-glow');
+
+  // submitButton.addEventListener('mousedown', (e) => {
+  //   submitButton.style.width = '98%';
+  //   submitButton.style.height = '46px';
+  //   console.log('mouse is down');
+  // });
+  // submitButton.addEventListener('mouseup', (e) => {
+  //   submitButton.style.width = '100%';
+  //   submitButton.style.height = '50px';
+  //   console.log('mouse is up');
+  // });
+  submitButton.addEventListener('click', (e) => {
+    confirmMessage.style.opacity = '1';
+    imageBack.style.opacity = '1';
+    submitButton.style.fontSize = '1.3rem';
+    setTimeout(() => {
+      confirmMessage.style.opacity = '0';
+    }, '5000');
+    setTimeout(() => {
+      imageBack.style.opacity = '0';
+    }, '670');
+    setTimeout(() => {
+      submitButton.style.fontSize = '1.2rem';
+    }, '200');
+  });
+};
+
+//Script manager for pages switching
+
+const pageManager = {
+  init: () => {
+    let page = document.body.id;
+    switch (page) {
+      case 'home':
+        console.log(page);
+        launchHome(page);
+        break;
+      case 'work':
+        console.log(page);
+        launchWork(page);
+        break;
+      case 'about':
+        console.log(page);
+        launchAbout(page);
+        break;
+      case 'contact':
+        console.log(page);
+        launchContact(page);
+        break;
+      default:
+      // launchHome(page);
+    }
+  },
+};
+
+pageManager.init();
---
+++ js/functions.js
@@ -0,0 +1,114 @@
+//Set global theme for light or dark mode
+export const setTheme = (themeSetting) => {
+  const themeButton = document.querySelector('#theme-switch');
+
+  if (themeSetting === 'dark') {
+    //Set to Dark
+    document.documentElement.style.setProperty('--color-text', 'white');
+    document.documentElement.style.setProperty('--color-background', '#131931');
+    document.documentElement.style.setProperty('--tab-color', '#191d2d');
+    // document.documentElement.style.setProperty('--form-shadow', '#131830');
+    document.querySelector('.logo-back').style.backgroundColor = '#131830';
+    document.querySelector('.logo-back').style.boxShadow =
+      '0 0 8px 1px var(--color-accent-medium)';
+    // document.querySelector('#cont-form').style.boxShadow =
+    //   '0 0 8px 1px var(--color-background)';
+    themeButton.style.backgroundColor = '#f8f8f8';
+    themeButton.style.color = '#131830';
+    themeButton.style.opacity = '0.8';
+    themeButton.innerText = 'LIGHT THEME';
+  } else {
+    //Set to Light
+    document.documentElement.style.setProperty('--color-text', '#131931');
+    document.documentElement.style.setProperty(
+      '--color-background',
+      'var(--gray-0)'
+    );
+    document.documentElement.style.setProperty('--tab-color', '#1f2952');
+
+    // document.documentElement.style.setProperty(
+    //   '--form-shadow',
+    //   'rgba(178, 177, 177, 0.638)'
+    // );
+
+    document.querySelector('.logo-back').style.backgroundColor =
+      'var(--color-accent-light)';
+    document.querySelector('.logo-back').style.boxShadow =
+      '0 0 8px 1px var(--gray-5)';
+    // document.querySelector('#cont-form').style.boxShadow =
+    //   '0 2px 12px 4px rgba(178, 177, 177, 0.638)';
+    themeButton.style.backgroundColor = '#1f2952';
+    themeButton.style.color = 'var(--gray-1)';
+    themeButton.style.opacity = '1.0';
+    themeButton.innerText = 'DARK THEME';
+  }
+};
+
+export const loadListeners = () => {
+  //Load universal elements
+  const navbar = document.querySelector('#navbar');
+  const navButton = document.querySelector('#nav-btn');
+  const menuIcon = document.querySelector('#menu-icon');
+  const topBar = document.querySelector('#top-bar');
+  const middleBar = document.querySelector('#middle-bar');
+  const bottomBar = document.querySelector('#bottom-bar');
+  const b1 = document.querySelector('#b1');
+  const b2 = document.querySelector('#b2');
+  const b3 = document.querySelector('#b3');
+  const header = document.querySelector('#header');
+
+  //Theme Switch Button
+  document.querySelector('#theme-switch').onclick = () => {
+    if (localStorage.getItem('theme') === 'dark') {
+      localStorage.setItem('theme', 'light');
+    } else {
+      localStorage.setItem('theme', 'dark');
+    }
+    setTheme(localStorage.getItem('theme'));
+  };
+
+  //Bottom border for fixed header
+  document.addEventListener('scroll', (e) => {
+    if (window.scrollY != 0) {
+      header.classList.add('header-border');
+      console.log(window.scrollY);
+    } else {
+      header.classList.remove('header-border');
+    }
+  });
+
+  navButton.addEventListener('click', () => {
+    //Open
+    if (navbar.dataset.open === 'false') {
+      navbar.dataset.open = 'true';
+      navbar.style.transform = 'translateX(0%)';
+
+      topBar.style.opacity = '0';
+      b1.className = 'menu-bar top-shift';
+      b2.style.opacity = '0';
+      b3.className = 'menu-bar bottom-shift';
+      bottomBar.style.opacity = '0';
+
+      // navbar.style.display = 'flex';
+      // navButton.style.display = 'block';
+      // middleBar.style.display = 'none';
+      // topBar.style.transform = 'rotate(45deg)';
+      // topBar.style.transformOrigin = 'top left';
+      // bottomBar.style.transform = 'rotate(-45deg)';
+      // bottomBar.className = 'menu-bar bottom-tilt';
+      // topBar.className = 'menu-bar top-tilt';
+      // menuIcon.className = 'fa-solid fa-xmark';
+    } else {
+      //Closed
+      navbar.dataset.open = 'false';
+      navbar.style.transform = 'translateX(100%)';
+      setTimeout(() => {
+        topBar.style.opacity = '1';
+        b1.className = '';
+        b2.style.opacity = '1';
+        b3.className = '';
+        bottomBar.style.opacity = '1';
+      }, 200);
+    }
+  });
+};
